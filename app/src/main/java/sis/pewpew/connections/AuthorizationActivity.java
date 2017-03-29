8d0a19c30f74f2ed79e4bc241d648c980b51e503
Vladislav Tarasov <vladislav.tarasov467@gmail.com>
2017-03-29 17:19:54 +0300

    Initial commit

---
+++ app/src/main/java/sis/pewpew/connections/AuthorizationActivity.java
@@ -0,0 +1,15 @@
+package sis.pewpew.connections;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sis.pewpew.R;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
